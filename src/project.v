fb31bc98a21dd5d5e821b2c0f70bb24fff1429dd
gojimmypi <gojimmypi@gmail.com>
2026-04-22 12:37:15 +0200

    correct param syntax

--- src/project.v
+++ src/project.v
@@ -11,7 +11,7 @@ module tt_um_gojimmypi (
 //    input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
 
-    input  wire s7:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
