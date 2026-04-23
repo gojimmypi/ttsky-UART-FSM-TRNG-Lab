d625e375dfdfc5ebc2cb4e33c38c997095a8f366
gojimmypi <gojimmypi@gmail.com>
2026-04-23 11:54:43 +0200

    pull out tt_um_uart_trng_ascii

--- src/lib.v
+++ src/lib.v
@@ -401,68 +401,6 @@ module trng_stub
 
 endmodule
 
-module tt_um_uart_trng_ascii
-(
-    input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
-    input  wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input  wire       ena,
-    input  wire       clk,
-    input  wire       rst_n
-);
-
-    localparam integer CLKS_PER_BIT = 217;
-
-    wire [7:0] reg_ctrl;
-    wire [7:0] reg_src;
-    wire [7:0] reg_div;
-    wire [7:0] reg_mode;
-    wire [7:0] reg_oscen;
-    wire [7:0] reg_status;
-    wire [7:0] reg_rawlo;
-    wire [7:0] reg_rawhi;
-    wire       trng_bit;
-    wire       uart_tx;
-
-    wire unused_ok;
-    assign unused_ok = &{ena, uio_in};
-
-    uart_trng_ascii_core
-    #(
-        .CLKS_PER_BIT(CLKS_PER_BIT)
-    )
-    u_core
-    (
-        .clk(clk),
-        .rst_n(rst_n),
-        .uart_rx_i(ui_in[3]),
-        .uart_tx_o(uart_tx),
-        .reg_ctrl_o(reg_ctrl),
-        .reg_src_o(reg_src),
-        .reg_div_o(reg_div),
-        .reg_mode_o(reg_mode),
-        .reg_oscen_o(reg_oscen),
-        .reg_status_o(reg_status),
-        .reg_rawlo_o(reg_rawlo),
-        .reg_rawhi_o(reg_rawhi),
-        .trng_bit_o(trng_bit)
-    );
-
-    assign uo_out[4] = uart_tx;
-    assign uo_out[0] = trng_bit;
-    assign uo_out[1] = reg_status[0];
-    assign uo_out[2] = reg_status[1];
-    assign uo_out[3] = reg_status[2];
-    assign uo_out[5] = reg_rawlo[0];
-    assign uo_out[6] = reg_rawlo[1];
-    assign uo_out[7] = reg_rawlo[2];
-
-    assign uio_out = reg_rawhi;
-    assign uio_oe  = 8'hFF;
-
-endmodule
 
 
 module ulx3s_uart_trng_ascii_top
