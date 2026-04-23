b4b7f910d443e51ca057c61772357ec716284322
gojimmypi <gojimmypi@gmail.com>
2026-04-23 11:53:21 +0200

    revert wrapper to tt_um_gojimmypi dut, echo working

--- ulx3s/top_ulx3s.v
+++ ulx3s/top_ulx3s.v
@@ -2,66 +2,65 @@
 `timescale 1ns/1ps
 
 module top_ulx3s (
-    input  wire       clk_25mhz,
-    input  wire [6:0] btn,
-    output wire [7:0] led,
-    input  wire       uart_rx_pin,
-    output wire       uart_tx_pin
+    input  wire        clk_25mhz,
+    input  wire [6:0]  btn,
+    output wire [7:0]  led,
+    input  wire        uart_rx_pin,
+    output wire        uart_tx_pin
 );
 
-    wire rst_n;
+    wire [7:0] ui_in;
+    wire [7:0] uio_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-    wire [7:0] reg_ctrl_o;
-    wire [7:0] reg_src_o;
-    wire [7:0] reg_div_o;
-    wire [7:0] reg_mode_o;
-    wire [7:0] reg_oscen_o;
-    wire [7:0] reg_status_o;
-    wire [7:0] reg_rawlo_o;
-    wire [7:0] reg_rawhi_o;
-    wire       trng_bit_o;
+    wire rst_n;
+    wire ena;
 
     reg uart_rx_meta;
     reg uart_rx_sync;
 
     assign rst_n = btn[0];
+    assign ena   = 1'b1;
 
     always @(posedge clk_25mhz) begin
         uart_rx_meta <= uart_rx_pin;
         uart_rx_sync <= uart_rx_meta;
     end
 
-    uart_trng_ascii_core
-    #(
-        .CLKS_PER_BIT(217)
-    )
-    u_core
+    // Map UART RX into TT input
+    assign ui_in = {4'b0000, uart_rx_sync, 3'b000};
+
+    assign uio_in = 8'h00;
+
+    tt_um_gojimmypi dut
     (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
         .clk(clk_25mhz),
-        .rst_n(rst_n),
-        .uart_rx_i(uart_rx_sync),
-        .uart_tx_o(uart_tx_pin),
-
-        .reg_ctrl_o(reg_ctrl_o),
-        .reg_src_o(reg_src_o),
-        .reg_div_o(reg_div_o),
-        .reg_mode_o(reg_mode_o),
-        .reg_oscen_o(reg_oscen_o),
-
-        .reg_status_o(reg_status_o),
-        .reg_rawlo_o(reg_rawlo_o),
-        .reg_rawhi_o(reg_rawhi_o),
-        .trng_bit_o(trng_bit_o)
+        .rst_n(rst_n)  // TODO - add a reset button and connect it here instead of hardcoding rst_n=1
     );
 
-//    assign led[0] = uart_rx_sync;
-//    assign led[1] = uart_tx_pin;
-//    assign led[2] = reg_status_o[1];
-//    assign led[3] = reg_status_o[2];
-//    assign led[4] = reg_status_o[3];
-//    assign led[5] = reg_rawlo_o[0];
-//    assign led[6] = reg_rawlo_o[1];
-//    assign led[7] = reg_rawlo_o[2];
+    `ifdef FORCE_LOOPBACK
+        // Loopback UART TX to RX for testing
+        initial $display("FORCE_LOOPBACK ENABLED");
+        assign uart_tx_pin = uart_rx_sync;  
+        error - we do not want loopback here!
+    `else
+        initial $display("FORCE_LOOPBACK DISABLED");
+        assign uart_tx_pin = uo_out[4];
+        // assign uart_tx_pin = 1'b0;
+        // assign uart_tx_pin = 1'b1;
+    `endif /* FORCE_LOOPBACK */
+
+    // Debug
+    assign led = uo_out;
+    // assign led = 8'h00;
 
 endmodule
 
