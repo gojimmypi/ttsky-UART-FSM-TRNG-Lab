420075125f7181c7517623d24dea621247e91610
gojimmypi <gojimmypi@gmail.com>
2026-05-21 18:55:28 +0200

    Code indenting, no other changes, confirmed ULX3S tests. lpf v20 for v3.0.7 85F

--- ulx3s/top_ulx3s.v
+++ ulx3s/top_ulx3s.v
@@ -55,14 +55,14 @@ module top_ulx3s (
 
     /* TODO: ULX3S JTAG wrapper */
 
-`endif
+`endif /* ULX3S_JTAG_ENABLED */
 
 /* Experimental RTS/DTS to control ESP32 boot mode during serial programming.
  * See also ESP32_BOOT_CONTROL_ENABLED, below  */
 `ifdef ESP32_BOOT_RTS_DTS_ENABLED
     input  wire        ftdi_nrts,
     input  wire        ftdi_ndtr,
-`endif
+`endif /* ESP32_BOOT_RTS_DTS_ENABLED */
     /* ESP32 UART and boot control. */
     output wire        wifi_rxd,
     input  wire        wifi_txd,
@@ -79,13 +79,14 @@ module top_ulx3s (
     /* The reference v20 lpf file contains BOTH gn12 AND gn[12]. To avoid complaints: */
     wire gn12;
     assign gn12 = \gn[12] ;
-`endif
+`endif /* ULX3S_BOARD_VERSION_v20 */
 
-    `ifdef ULX3S_USE_GN12_50MHZ
-        assign clk_ulx3s = gn12;
-    `else
-        assign clk_ulx3s = clk_25mhz;
-    `endif
+`ifdef ULX3S_USE_GN12_50MHZ
+    /* Reminder this 50 MHz clock ONLY comes from HDMI */
+    assign clk_ulx3s = gn12;
+`else
+    assign clk_ulx3s = clk_25mhz;
+`endif
 
     wire [7:0] ui_in;
     wire [7:0] uio_in;
@@ -117,7 +118,7 @@ module top_ulx3s (
     wire spi_mosi; /* ESP32 PIN_NUM_MOSI 15 */
     wire spi_cs_n; /* ESP32 PIN_NUM_CS   13 */
     wire spi_miso; /* ESP32 PIN_NUM_MISO  2 */
-`endif
+`endif /* ULX3S_SPI_ENABLED */
 
     /* The BTN0 "PWR" on the ULX3S is used for reset. 
      * It is active-low, so we can connect it directly to rst_n. */
@@ -125,55 +126,55 @@ module top_ulx3s (
 
     assign ena   = 1'b1;
 
-    `ifdef ESP32_BOOT_CONTROL_ENABLED
-        /* If ESP32_BOOT_CONTROL_ENABLED is defined, BTN0 controls wifi_en and BTN1 controls wifi_gpio0 
-         *
-         * To RESET the ESP32 and start the running program in flash:
-         *    Hold btn[1]
-         *    Tap btn[0]
-         *    Release btn[1] 
-         *
-         * To PROGRAM the ESP32 in flash:
-         *    Hold btn[0]
-         *      (begin flash upload)
-         *    Release btn[0] when "Connecting..." is observed.
-         * 
-         * Should then see something like:
-         *
-         *   Chip is ESP32-D0WDQ6 (revision v1.0)
-         *   Features: WiFi, BT, Dual Core, 240MHz, VRef calibration in efuse, Coding Scheme None
-         *   Crystal is 40MHz
-         *   Uploading stub...
-         *   Running stub...
-         *   Stub running...
-         *   Changing baud rate to 460800
-         *   Changed.
-         */
-        `ifdef ESP32_BOOT_RTS_DTS_ENABLED
-            wire dtr;
-            wire rts;
+`ifdef ESP32_BOOT_CONTROL_ENABLED
+    /* If ESP32_BOOT_CONTROL_ENABLED is defined, BTN0 controls wifi_en and BTN1 controls wifi_gpio0 
+        *
+        * To RESET the ESP32 and start the running program in flash:
+        *    Hold btn[1]
+        *    Tap btn[0]
+        *    Release btn[1] 
+        *
+        * To PROGRAM the ESP32 in flash:
+        *    Hold btn[0]
+        *      (begin flash upload)
+        *    Release btn[0] when "Connecting..." is observed.
+        * 
+        * Should then see something like:
+        *
+        *   Chip is ESP32-D0WDQ6 (revision v1.0)
+        *   Features: WiFi, BT, Dual Core, 240MHz, VRef calibration in efuse, Coding Scheme None
+        *   Crystal is 40MHz
+        *   Uploading stub...
+        *   Running stub...
+        *   Stub running...
+        *   Changing baud rate to 460800
+        *   Changed.
+        */
+    `ifdef ESP32_BOOT_RTS_DTS_ENABLED
+        wire dtr;
+        wire rts;
 
-            wire en_auto;
-            wire gpio0_auto;
+        wire en_auto;
+        wire gpio0_auto;
 
-            assign dtr = ~ftdi_ndtr;
-            assign rts = ~ftdi_nrts;
+        assign dtr = ~ftdi_ndtr;
+        assign rts = ~ftdi_nrts;
 
-            assign en_auto    = ~(rts & ~dtr);
-            assign gpio0_auto = ~(dtr & ~rts);
+        assign en_auto    = ~(rts & ~dtr);
+        assign gpio0_auto = ~(dtr & ~rts);
 
-            assign wifi_en    = en_auto;
-            assign wifi_gpio0 = gpio0_auto;
-        `else
-            /* Current default: no RTS / DTS control */
-            assign wifi_en    = btn[0];
-            assign wifi_gpio0 = btn[1];
-        `endif /* ESP32_BOOT_RTS_DTS_ENABLED */
+        assign wifi_en    = en_auto;
+        assign wifi_gpio0 = gpio0_auto;
     `else
-        /* Keep ESP32 enabled and in normal boot mode. */
-        assign wifi_en    = 1'b1;
-        assign wifi_gpio0 = 1'b1;
-    `endif /* ESP32_BOOT_CONTROL_ENABLED */
+        /* Current default: no RTS / DTS control */
+        assign wifi_en    = btn[0];
+        assign wifi_gpio0 = btn[1];
+    `endif /* ESP32_BOOT_RTS_DTS_ENABLED */
+`else
+    /* Keep ESP32 enabled and in normal boot mode. */
+    assign wifi_en    = 1'b1;
+    assign wifi_gpio0 = 1'b1;
+`endif /* ESP32_BOOT_CONTROL_ENABLED */
 
     /* Do not shut down ULX3S power. */
     assign shutdown = 1'b0;
@@ -184,61 +185,63 @@ module top_ulx3s (
      * Define ESP32_UART_ENABLED to connect the TT UART to the onboard ESP32.
      * Otherwise, keep using gp0/gp1 for the external UART path.
      */
-    `ifdef ESP32_UART_ENABLED
-        assign uart_rx_pin = wifi_txd;
-        assign wifi_rxd    = uart_tx_pin;
+`ifdef ESP32_UART_ENABLED
+    assign uart_rx_pin = wifi_txd;
+    assign wifi_rxd    = uart_tx_pin;
+    assign gp1         = uart_tx_pin;
+
+    /* Mirror ESP32 TX to the USB FTDI RX pin for debug visibility. */
+    assign ftdi_rxd = wifi_txd;
+
+    wire unused_ftdi_txd;
+    assign unused_ftdi_txd = ftdi_txd;
+`else
+    /* no ESP32_UART_ENABLED */
+    `ifdef NO_ESP32_PASSTHRU_ENABLED
+        /* The ULX3S US1 USB port is NOT connected to the ESP32 */
+    `else
+        /* Unless explicitly disabled or otherwise assigned, connect the ESP32 to the ULX3S port */
+        assign uart_rx_pin = gp0;
         assign gp1         = uart_tx_pin;
 
-        /* Mirror ESP32 TX to the USB FTDI RX pin for debug visibility. */
+        /* Leave USB FTDI connected to ESP32 when not using ESP32 for TT UART. (see above) */
+        assign wifi_rxd = ftdi_txd;
         assign ftdi_rxd = wifi_txd;
+    `endif /* ESP32_PASSTHRU_ENABLED */
+`endif /* ESP32_UART_ENABLED */
 
-        wire unused_ftdi_txd;
-        assign unused_ftdi_txd = ftdi_txd;
+/* Optional UART support - enable by defining UART_ENABLED in your project.v */
+`ifdef UART_ENABLED
+    /* See example UART: https://github.com/gojimmypi/ttsky-UART-FSM-TRNG-Lab */
+
+    always @(posedge clk_ulx3s) begin
+        uart_rx_meta <= uart_rx_pin;
+        uart_rx_sync <= uart_rx_meta;
+    end
+
+    // Map UART RX into TT input
+    assign ui_in = {4'b0000, uart_rx_sync, 3'b000};
+
+`endif /* UART_ENABLED */
+
+`ifdef ULX3S_SPI_ENABLED
+    assign uio_in = {4'b0000, spi_sck, 1'b0, spi_mosi, spi_cs_n};
+    assign spi_sck    = wifi_gpio14;
+    assign spi_mosi   = wifi_gpio15;
+    assign spi_cs_n   = wifi_gpio13;
+
+    `ifdef TEST_SPI_ZERO
+        /* Board-level pin test before using the TT SPI implementation. */
+        assign wifi_gpio2 = 1'b0;  /* ESP32 should return rx: 00 00 */
+        // assign wifi_gpio2 = 1'b1;  /* ESP32 should return rx: FF FF */
     `else
-        `ifdef NO_ESP32_PASSTHRU_ENABLED
-            /* The ULX3S US1 USB port is NOT connected to the ESP32 */
-        `else
-            /* Unless explicitly disabled or otherwise assigned, connect the ESP32 to the ULX3S port */
-            assign uart_rx_pin = gp0;
-            assign gp1         = uart_tx_pin;
-
-            /* Leave USB FTDI connected to ESP32 when not using ESP32 for TT UART. (see above) */
-            assign wifi_rxd = ftdi_txd;
-            assign ftdi_rxd = wifi_txd;
-        `endif
-    `endif /* ESP32_UART_PASSTHRU_ENABLED */
-
-    /* Optional UART support - enable by defining UART_ENABLED in your project.v */
-    `ifdef UART_ENABLED
-        /* See example UART: https://github.com/gojimmypi/ttsky-UART-FSM-TRNG-Lab */
-
-        always @(posedge clk_ulx3s) begin
-            uart_rx_meta <= uart_rx_pin;
-            uart_rx_sync <= uart_rx_meta;
-        end
-
-        // Map UART RX into TT input
-        assign ui_in = {4'b0000, uart_rx_sync, 3'b000};
-
-    `endif /* UART_ENABLED */
-
-    `ifdef ULX3S_SPI_ENABLED
-        assign uio_in = {4'b0000, spi_sck, 1'b0, spi_mosi, spi_cs_n};
-        assign spi_sck    = wifi_gpio14;
-        assign spi_mosi   = wifi_gpio15;
-        assign spi_cs_n   = wifi_gpio13;
-
-        `ifdef TEST_SPI_ZERO
-            /* Board-level pin test before using the TT SPI implementation. */
-            assign wifi_gpio2 = 1'b0;  /* ESP32 should return rx: 00 00 */
-            // assign wifi_gpio2 = 1'b1;  /* ESP32 should return rx: FF FF */
-        `else
-            assign spi_miso   = uio_out[2];
-            assign wifi_gpio2 = spi_miso;
-        `endif
-    `else
-        assign uio_in = 8'h00;
+        assign spi_miso   = uio_out[2];
+        assign wifi_gpio2 = spi_miso;
     `endif
+`else
+    /* ULX3S_SPI_ENABLED not enabled. TODO what about JTAG?  */
+    assign uio_in = 8'h00;
+`endif
 
     /*************************************************************************
      * Instantiate the main DUT from TT module in /project.v
@@ -261,19 +264,19 @@ module top_ulx3s (
         .rst_n(rst_n)  // TODO - add a reset button and connect it here instead of hardcoding rst_n=1
     );
 
-    /* Some optional UART loopback test for development only */
-    `ifdef FORCE_LOOPBACK
-        // Loopback UART TX to RX for testing
-        initial $display("FORCE_LOOPBACK ENABLED");
-        assign uart_tx_pin = uart_rx_sync;
+/* Some optional UART loopback test for development only */
+`ifdef FORCE_LOOPBACK
+    // Loopback UART TX to RX for testing
+    initial $display("FORCE_LOOPBACK ENABLED");
+    assign uart_tx_pin = uart_rx_sync;
 
-        // Optionally ensure your project is not submitted in loopback mode
-        // MODULE_FORCE_LOOPBACK_MUST_NOT_BE_ENABLED u_stop ();
+    // Optionally ensure your project is not submitted in loopback mode
+    // MODULE_FORCE_LOOPBACK_MUST_NOT_BE_ENABLED u_stop ();
 
-    `else
-        initial $display("FORCE_LOOPBACK DISABLED");
-        assign uart_tx_pin = uo_out[4];
-    `endif /* FORCE_LOOPBACK */
+`else
+    initial $display("FORCE_LOOPBACK DISABLED");
+    assign uart_tx_pin = uo_out[4];
+`endif /* FORCE_LOOPBACK */
 
     // Optional Debug
 `ifdef ULX3S_CLOCK_TEST
