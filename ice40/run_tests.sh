3a0500c3b2378b2a719f0963d48f20e4e1eb512f
gojimmypi <gojimmypi@gmail.com>
2026-06-08 17:03:42 -0700

    Explicit TT_UART_PORT reference setting

--- ice40/run_tests.sh
+++ ice40/run_tests.sh
@@ -6,22 +6,29 @@
 # file: ice40/run_tests.sh
 #
 
-UART_PORT=/dev/ttyS5
 set -e
 set -o pipefail
 
+echo "**************************************************************************"
+echo "**  Begin ${BASH_SOURCE[0]} from ${PWD}"
+echo "**************************************************************************"
 
-# The TT port is the board with the repl prompt, NOT the connected UART
+# The TT_UART_PORT is the external USB TTY device, typically connected to IN3/Rx and OUT4/Tx.
+MY_TT_UART_PORT=/dev/ttyS5
+
+export TT_UART_PORT=${MY_TT_UART_PORT}
+
+# WARNING: This is NOT the TT_PORT for the Tiny Tapeout Demoboard.
+# This port is the external UART connected to the PMOD pins!
+    echo "TT_UART_PORT: ${TT_UART_PORT}"
+
+# The TT port is the board with the repl prompt, NOT the connected external UART adapter.
 if [ -z "$TT_PORT" ]; then
     echo  "info: no TT_PORT found" >&2
     exit 1
 else
-    echo "TT_PORT:   ${TT_PORT}"
+    echo "TT_PORT:      ${TT_PORT}"
 fi
-
-# WARNING: This is NOT the TT_PORT for the Tiny Tapeout Demoboard.
-# This port is the external UART connected to the PMOD pins!
-echo "UART_PORT: ${UART_PORT}"
 
 # Run shell check to ensure this a good script.
 # Specify the executable shell checker you want to use:
@@ -37,26 +44,32 @@ fi
 
 # Setup environment
 echo "**************************************************************************"
-echo " Setup environment"
+echo "**  Setup environment"
 echo "**************************************************************************"
 . ./env_ice40.sh
 
 echo "**************************************************************************"
-echo "Calling project reset script on port ${TT_PORT}"
+echo "**  Calling project reset script on port ${TT_PORT}"
 echo "**************************************************************************"
 ./project_reset.sh || exit 1
 
 
 echo "**************************************************************************"
-echo "Calling test scripts on port ${UART_PORT}"
+echo "**  Calling test scripts on port ${TT_UART_PORT}"
 echo "**************************************************************************"
 
-python ../test-hw/tt_ulx3s_uart_test.py --port "$UART_PORT"                   || exit 1
+python ../test-hw/tt_ulx3s_uart_test.py --port "$TT_UART_PORT"                   || exit 1
 
-python ../test-hw/tt_ulx3s_uart_test.py --port "$UART_PORT" --reset-registers || exit 1
+python ../test-hw/tt_ulx3s_uart_test.py --port "$TT_UART_PORT" --reset-registers || exit 1
 
-python ../test-hw/tt_ulx3s_trng_uart_test.py --port "$UART_PORT"              || exit 1
+python ../test-hw/tt_ulx3s_trng_uart_test.py --port "$TT_UART_PORT"              || exit 1
 
-python ../test-hw/tt_ulx3s_trng_repro_test.py --port  "$UART_PORT"            || exit 1
+python ../test-hw/tt_ulx3s_trng_repro_test.py --port  "$TT_UART_PORT"            || exit 1
 
-python ../test-hw/tt_ulx3s_uart_test.py --port "$UART_PORT" --reset-registers || exit 1
+python ../test-hw/tt_ulx3s_uart_test.py --port "$TT_UART_PORT" --reset-registers || exit 1
+
+echo "**************************************************************************"
+echo "**  Done. Ports used:"
+echo "**************************************************************************"
+echo "TT_UART_PORT: ${TT_UART_PORT}"
+echo "TT_PORT:      ${TT_PORT}"
