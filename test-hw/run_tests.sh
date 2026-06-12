cc094eee4465349cbbd79c061f236cac9ac88265
gojimmypi <gojimmypi@gmail.com>
2026-06-12 13:44:47 -0700

    optionally capture binary file at build time

--- test-hw/run_tests.sh
+++ test-hw/run_tests.sh
@@ -304,7 +304,9 @@ else
     python ./tt_ulx3s_trng_repro_test.py --port  "$PORT"            || exit 1
 fi
 
+echo "Generating a 16MB trng_conditioned.bin"
+
 # Generate a bin file of random data to be analyzed
-./capture_trng_raw_uart.py  --port "$PORT"  --bytes 16777216  --out trng_conditioned.bin  --fast-baud  --conditioned
+# ./capture_trng_raw_uart.py  --port "$PORT"  --bytes 16777216  --out trng_conditioned.bin  --fast-baud  --conditioned
 
 echo "Port used: $PORT"
