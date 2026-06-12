6f697dfb53e488af4cfc7c11c9cf2a71a3d82c98
gojimmypi <gojimmypi@gmail.com>
2026-06-12 10:53:26 -0700

    Revert version string after unexpected size reduction

--- src/project_config.v
+++ src/project_config.v
@@ -24,7 +24,7 @@
 
     `ifdef USE_LONG_STRINGS
         `define VERSION_STRING_LEN 24 /* 123456789012345678901234 */   
-        `define VERSION_STRING          "Version 0.1.7e 6/11/2026"
+        `define VERSION_STRING          "Version 0.1.7d 6/11/2026"
     `else
         /* no long strings */
     `endif
